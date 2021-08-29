8051a216047af89b5cc56c78096fdc35e24da04a
vikDang <dvtd2000@gmail.com>
2021-08-30 02:02:20 +0700

    Fix bugs and shorten code

--- main.js
+++ main.js
@@ -2,48 +2,58 @@ var courses = [
     {
         id: 1,
         name: 'Javascript',
-        coin: 250
+        coin: 250,
+        isFinish: false
     },
     {
         id: 2,
         name: 'HTML, CSS',
-        coin: 0
+        coin: 0,
+        isFinish: true
     },
     {
         id: 3,
         name: 'Ruby',
-        coin: 0
+        coin: 0,
+        isFinish: false
     },
     {
         id: 4,
         name: 'PHP',
-        coin: 400
+        coin: 400,
+        isFinish: false
     },
     {
         id: 5,
         name: 'ReactJS',
-        coin: 500
+        coin: 500,
+        isFinish: false
     },
     {
         id: 6,
-        name: 'PHP',
-        coin: 400
+        name: 'NodeJS',
+        coin: 800,
+        isFinish: false
     },
     {
         id: 7,
-        name: 'PHP',
-        coin: 400
+        name: 'Java',
+        coin: 600,
+        isFinish: false
     },
 ]
 
 
 //********Add Array forEach2() method === forEach() method
 Array.prototype.forEach2 = function(callback) {
-    var arrayLength = this.length;
-    for (let i = 0; i < arrayLength; i++) {
-        callback(this[i], i, this);
+    for (var index in this) {
+        if (this.hasOwnProperty(index)) {
+            callback(this[index], index, this);
+        }
     }
 }
+
+// courses = new Array(100);
 
 // courses.forEach2(function(course) {
 //     console.log(course)
@@ -58,23 +68,52 @@ Array.prototype.forEach2 = function(callback) {
 
 //*******Add Array every2() method === every() method
 Array.prototype.every2 = function(callback) {
-    var arrayLength = this.length;
-    for(let i = 0; i < arrayLength; i++) {
-        var isRight = callback(this[i], i, this);
-        if (!!isRight) {
-            continue;
-        } else {
-            return !!isRight;
+    var output = true;
+    for(var index in this) {
+        if (this.hasOwnProperty(index)) {
+            var result = callback(this[index], index, this);
+            if (!result) {
+                output = false;
+            }
         }
     }
-    return !!isRight
+    return output;
 }
 
-// var isFree = courses.every2(function(course) {
-//     return course === 0;
+// var result = courses.every2(function(course) {
+//     return course.coin >= 200;
 // })
 
-// console.log(isFree)
+// console.log(result)
+
+
+
+
+
+
+
+
+//*******Add Array some2() method === some() method
+Array.prototype.some2 = function(callback) {
+    var output = false;
+    for(var index in this) {
+        if (this.hasOwnProperty(index)) {
+            if (callback(this[index], index, this)) {
+                output = true;
+                break;
+            }
+        }
+    }
+    return output;
+}
+
+// var isFinish = courses.some2(function(course) {
+//     return course.isFinish;
+// })
+
+// console.log(isFinish)
+
+
 
 
 
@@ -82,19 +121,18 @@ Array.prototype.every2 = function(callback) {
 
 //*******Add Array find2() method === find() method
 Array.prototype.find2 = function(callback) {
-    var arrayLength = this.length;
-    for(let i = 0; i < arrayLength; i++) {
-        var item = callback(this[i], i, this);
-        if (item) {
-            return this[i];
-        } else {
-            continue;
+    for(var index in this) {
+        if (this.hasOwnProperty(index)) {
+            var result = callback(this[index], index, this);
+            if (result) {
+                return this[index];
+            }
         }
     }
 }
 
 // var course = courses.find2(function(course) {
-//     return course.name === 'PHP';
+//     return course.name === 'Ruby';
 // })
 
 // console.log(course)
@@ -106,21 +144,22 @@ Array.prototype.find2 = function(callback) {
 
 //*******Add Array filter2() method === filter() method
 Array.prototype.filter2 = function(callback) {
-    var arrayLength = this.length;
-    var myArray = [];
-    for(let i = 0; i < arrayLength; i++) {
-        var item = callback(this[i], i, this);
-        if (item) {
-            myArray.push(this[i]);
-        } else {
-            continue;
+    var output = [];
+
+    for(var index in this) {
+        if (this.hasOwnProperty(index)) {
+            var result = callback(this[index], index, this);
+            if (result) {
+                output.push(this[index]);
+            }
         }
     }
-    return myArray;
+    
+    return output;
 }
 
 // var course = courses.filter2(function(course) {
-//     return course.coin === 500;
+//     return course.coin >= 00;
 // })
 
 // console.log(course)
@@ -131,17 +170,20 @@ Array.prototype.filter2 = function(callback) {
 
 //*******Add Array map2() method === map() method
 Array.prototype.map2 = function(callback) {
-    var arrayLength = this.length;
-    var myArray = [];
-    for(let i = 0; i < arrayLength; i++) {
-        var item = callback(this[i], i, this);
-        myArray.push(item);
+    var output = [];
+
+    for(var index in this) {
+        if (this.hasOwnProperty(index)) {
+            var result = callback(this[index], index, this);
+            output.push(result);
+        }
     }
-    return myArray;
+    
+    return output;
 }
 
 // var course = courses.map2(function(course) {
-//     return course.coin;
+//     return course.name;
 // })
 
 // console.log(course)
@@ -150,19 +192,20 @@ Array.prototype.map2 = function(callback) {
 
 //*******Add Array reduce2() method === reduce() method
 Array.prototype.reduce2 = function(callback, result) {
-    let i = 0
-    if(arguments.length < 2) {
-        i = 1;
-        result = this[0];
-    }
-    var arrayLength = this.length;
-    for(; i < arrayLength; i++) {
-        result = callback(result, this[i], i, this);
+    for(var index in this) {
+        if (this.hasOwnProperty(index)) {
+            if(arguments.length < 2 && index == 0) {
+                result = this[0];
+                continue;
+            }
+            result = callback(result, this[index], index, this);
+        }
     }
     return result;
 }
 
-var numbers = [100, 200, 500, 300, 700, 900]
+var numbers1 = [100, 200, 500, 300, 700, 900]
+var numbers = [1,2,3]
 
 var total = numbers.reduce2(function(total, number) {
     return total + number;
